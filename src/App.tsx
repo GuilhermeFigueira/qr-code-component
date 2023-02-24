088af1bd7e58ef14f9c19545f6a6deb38563047a
Guilherme Figueira <guibf2004@gmail.com>
2023-02-24 19:49:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+	return (
+		<>
+			<strong>Improve your front-end skills by building projects</strong>
+			<span>
+				Scan the QR code to visit Frontend Mentor and take your coding
+				skills to the next level
+			</span>
+			<div>
+				Challenge by{" "}
+				<a
+					href="https://www.frontendmentor.io?ref=challenge"
+					target="_blank"
+				>
+					Frontend Mentor
+				</a>
+				. Coded by{" "}
+				<a href="https://github.com/guilhermefigueira" target="_blank">
+					Guilherme Figueira
+				</a>
+				.
+			</div>
+		</>
+	);
+}
+
+export default App;
