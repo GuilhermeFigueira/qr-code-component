503cb5be3481125e006e23ad132806b353f6c6ea
Guilherme Figueira <guibf2004@gmail.com>
2023-02-24 21:19:23 -0300

    finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 function App() {
 	return (
-		<>
-			<strong>Improve your front-end skills by building projects</strong>
-			<span>
-				Scan the QR code to visit Frontend Mentor and take your coding
-				skills to the next level
-			</span>
-			<div>
-				Challenge by{" "}
-				<a
-					href="https://www.frontendmentor.io?ref=challenge"
-					target="_blank"
-				>
-					Frontend Mentor
-				</a>
-				. Coded by{" "}
-				<a href="https://github.com/guilhermefigueira" target="_blank">
-					Guilherme Figueira
-				</a>
-				.
-			</div>
-		</>
+		<div className="flex h-full">
+			<main className="bg-white w-80 rounded-2xl p-4 items-center m-auto  h-fit shadow-lg ">
+				<img
+					src="./src/assets/image-qr-code.png"
+					alt="QR Code"
+					className="rounded-2xl "
+				/>
+				<footer className="p-6 flex flex-col gap-3">
+					<strong className="font-bold text-md text-blue-700 text-center">
+						Improve your front-end skills by building projects
+					</strong>
+					<span className="font-normal text-blue-300 text-center">
+						Scan the QR code to visit Frontend Mentor and take your
+						coding skills to the next level
+					</span>
+					<div className="text-center">
+						Challenge by{" "}
+						<a
+							href="https://www.frontendmentor.io?ref=challenge"
+							target="_blank"
+						>
+							Frontend Mentor
+						</a>
+						. Coded by{" "}
+						<a
+							href="https://github.com/guilhermefigueira"
+							target="_blank"
+							className="text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-sky-700 font-bold hover:text-lg transition-all"
+						>
+							Guilherme Figueira
+						</a>
+						.
+					</div>
+				</footer>
+			</main>
+		</div>
 	);
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
 	content: ["./src/**/*.tsx", "./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: "hsl(0, 0%, 100%)",
+				gray: "hsl(212, 45%, 89%)",
+				blue: {
+					300: "hsl(220, 15%, 55%)",
+					700: "hsl(218, 44%, 22%)",
+				},
+			},
+			fontSize: {
+				md: "20px",
+			},
+			fontFamily: {
+				Outfit: ["Outfit", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
